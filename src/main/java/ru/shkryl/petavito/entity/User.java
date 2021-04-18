0d1fc479d95185d438a732c2a33c32cc81323de7
Shkryl Andrei <ShkrylAndrei@rambler.ru>
2021-04-18 18:40:01 +0400

    add entity userinfo, not finish yet

---
+++ src/main/java/ru/shkryl/petavito/entity/User.java
@@ -0,0 +1,26 @@
+package ru.shkryl.petavito.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Table(name = "userinfo")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+}
