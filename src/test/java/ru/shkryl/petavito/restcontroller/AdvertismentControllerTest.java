cd121bf173cfef224fb17e921d770807d6bcd910
Shkryl Andrei <ShkrylAndrei@rambler.ru>
2021-07-25 15:19:03 +0400

    add first text and src for link which description how to test

---
+++ src/test/java/ru/shkryl/petavito/restcontroller/AdvertismentControllerTest.java
@@ -0,0 +1,10 @@
+package ru.shkryl.petavito.restcontroller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+//Example
+//https://javahowtos.com/guides/123-testing/353-test-controller-in-spring-using-mockito-and-mockmvc-example.html
+class AdvertismentControllerTest {
+
+}
